b208a6007d038044c56498833e6922291b8a0238
kaziashik <kazia096@gmail.com>
2021-10-06 15:37:56 +0800

    load countrues using useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Countries></Countries>
+
+
     </div>
   );
+}
+
+
+function Countries() {
+  const [countries, setcountries] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setcountries(data));
+  }, [])
+  return (
+    <div>
+      <h2>Traveling around the world!!</h2>
+      <h4>Countries Available:{countries.length}</h4>
+    </div>
+  )
 }
 
 export default App;
